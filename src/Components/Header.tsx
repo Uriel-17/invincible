b6fd31340533768b0fa130a4aabb0adb6b9b7760
Uriel Garcia <urielgarcia@MacBook-Pro.local>
2026-01-23 20:25:58 -0800

    header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,12 +1,23 @@
 import './Styles/Header.css'
+import { useTranslation } from 'react-i18next'
+import { Link } from '@tanstack/react-router'
 import ThemeToggle from './HeaderComponents/ThemeToggle'
 import ClearMonthBtn from './HeaderComponents/ClearMonthBtn'
+import SwitchLanguageBtn from './HeaderComponents/SwitchLanguageBtn'
 
 const Header = () => {
+  const { t } = useTranslation()
+
   return (
     <header className="app-header">
-      <ClearMonthBtn/>
-      <ThemeToggle />
+      <Link className="app-header-title" to="/">
+        {t('appName')}
+      </Link>
+      <div className="app-header-actions">
+        <SwitchLanguageBtn />
+        <ClearMonthBtn />
+        <ThemeToggle />
+      </div>
     </header>
   )
 }
