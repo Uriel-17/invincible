f9bddc1b212b6591a62a01d40d1d328cdcb5c2ed
Uriel Garcia <urielgarcia@MacBook-Pro.local>
2026-02-01 11:24:02 -0800

    tanstack query key file

---
+++ src/queryKeys.ts
@@ -0,0 +1,11 @@
+/**
+ * Centralized TanStack Query Keys
+ *
+ * This file contains all query keys used throughout the application.
+ * Using constants ensures consistency and makes refactoring easier.
+ *
+ * Naming convention: [FEATURE]_QC_KEY (QC = Query Client)
+ */
+
+export const FIRST_LAUNCH_QC_KEY = 'firstLaunch'
+export const USER_SETTING_QC_KEY = 'userSetting'
