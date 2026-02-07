6a85aaa859ea54f5b54c8a6edb8899ea053c5e5c
Uriel Garcia <urielgarcia@MacBook-Pro.local>
2026-02-07 08:11:23 -0800

    Updating welcome page

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './i18n'
-import { RouterProvider } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import AppWithOnboarding from './AppWithOnboarding'
 import router from './router'
 
 const queryClient = new QueryClient({
@@ -23,7 +23,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AppWithOnboarding router={router} />
     </QueryClientProvider>
   </StrictMode>,
 )
