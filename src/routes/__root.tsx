ae31702969a5c11ea19f22fb3bc9f6aee5de13d8
Uriel Garcia <urielgarcia@MacBook-Pro.local>
2026-03-21 16:16:10 -0700

    fix: update Header import path after refactor

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,5 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
-import Header from 'src/Components/Header'
+import Header from 'src/Components/Header/Header'
 
 export const Route = createRootRoute({
   component: () => (
